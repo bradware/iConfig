ec2334cde03291b5d4c9df18b1ac674f383f0d99
Brad Ware <bware@apple.com>
2014-07-22 11:22:40 -0700

    select menu modifications

--- test/karma/unit/rest/addData_spec.js
+++ test/karma/unit/rest/addData_spec.js
@@ -3,7 +3,7 @@ var frisby = require('frisby');
 
 for(var count = 0; count < 1000; count++) {
     frisby.create('New Asset POST')
-    .post('http://localhost:3000/apps/53b06ea66d79fb15f51f2b8c/assets', 
+    .post('http://localhost:3000/apps/53c9542826d6fc0000774b7f/assets', 
     {
             name: 'testAsset-' + count,
             status: 'Not Translated',
@@ -11,6 +11,7 @@ for(var count = 0; count < 1000; count++) {
             tags: ['dummy', 'test'],
             created_by: 'BradWare',
             last_modified_by: 'BWare',
+            description: 'this is for testing purposes'
     })
     .toss();
 }
